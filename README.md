a955282b700f88888a068ce5e7a2f49114050f67
guswns2 <wkdguswns6@gmail.com>
2024-03-19 19:54:08 +0900

    HomeAbout 화면 CSS 정리

--- README.md
+++ README.md
@@ -18,16 +18,31 @@ Node.js: 20.11.0
 npm install (프로젝트 처음 받았을 때 한 번만 해주면 됨)
 npm run dev
 
-# Extension 설치
-
-Prettier, ESLint, Vue 3 Snippets
-
 # Figma Builder.io 플러그인
 
 설계한 UI 화면 Vue 코드 카피
 
 # IntelliJ - GitHub 연동
+
 https://brunch.co.kr/@mystoryg/168
 
 # IntelliJ - Pull Request
+
 https://ddoriya.tistory.com/entry/IntelliJ-Git-pull-request-%ED%95%98%EA%B8%B0
+
+# IntelliJ 얼티밋 버전 설치
+
+커뮤니티 버전은 Vue, TypeScript 관련 플러그인 지원 안 함..
+
+# IntelliJ 플러그인
+
+Grep Console - 콘솔 색상 구분
+
+# Element-Plus 설치 및 적용
+
+https://element-plus.org/en-US/guide/installation.html
+https://element-plus.org/en-US/guide/quickstart.html
+
+# IntelliJ - 파일 저장 시 코드 정렬
+
+설정 - 도구 - 저장 시 액션 - 코드 서식 다시 지정, Prettier 실행
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,43 +1,49 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeAbout from '@/views/HomeAbout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeAbout
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/LoginPage.vue')
-    },
-    {
-      path: '/main',
-      name: 'main',
-      component: () => import('../views/MainChallenge.vue')
-    },
-    {
-      path: '/create',
-      name: 'create',
-      component: () => import('../views/CreateChallenge.vue')
-    },
-    {
-      path: '/detail',
-      name: 'detail',
-      component: () => import('../views/DetailChallenge.vue')
-    },
-    {
-      path: '/mypage1',
-      name: 'mypage1',
-      component: () => import('../views/MyPage1.vue')
-    },
-    {
-      path: '/mypage2',
-      name: 'mypage2',
-      component: () => import('../views/MyPage2.vue')
+      name: 'layout',
+      component: () => import('../layouts/ChallengeLayout.vue'),
+      children: [
+        {
+          path: '/home',
+          name: 'home',
+          component: () => import('../views/HomeAbout.vue')
+        },
+        {
+          path: '/login',
+          name: 'login',
+          component: () => import('../views/LoginPage.vue')
+        },
+        {
+          path: '/main',
+          name: 'main',
+          component: () => import('../views/MainChallenge.vue')
+        },
+        {
+          path: '/create',
+          name: 'create',
+          component: () => import('../views/CreateChallenge.vue')
+        },
+        {
+          path: '/detail',
+          name: 'detail',
+          component: () => import('../views/DetailChallenge.vue')
+        },
+        {
+          path: '/mypage1',
+          name: 'mypage1',
+          component: () => import('../views/MyPage1.vue')
+        },
+        {
+          path: '/mypage2',
+          name: 'mypage2',
+          component: () => import('../views/MyPage2.vue')
+        }
+      ]
     }
   ]
 })
